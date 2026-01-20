9905bf8aeda165f66929ac4e70e0801ca8487689
JENNY KRUPAL MYLABATHULA <23a91a05b4@aec.edu.in>
2026-01-20 23:13:09 +0530

    Time: 0 ms (100%), Space: 43.3 MB (97.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int j = 0, i = m; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
+       int a=m-1;
+       int b=n-1;
+       int index = nums1.length-1;
+       while(a>=0 && b>=0){
+        if(nums1[a] >= nums2[b]){
+            nums1[index] = nums1[a];
+            a--;
+        }else{
+            nums1[index] = nums2[b];
+            b--;
         }
-        Arrays.sort(nums1);
+        index--;
+       }
+       while(b >=0){
+        nums1[index] = nums2[b];
+        index--;
+        b--;
+       }
     }
 }
