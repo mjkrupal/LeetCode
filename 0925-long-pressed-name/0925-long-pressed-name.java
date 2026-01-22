ea2ef9f9137c1ae1a0fceda3a7ee7c9f1280b660
JENNY KRUPAL MYLABATHULA <23a91a05b4@aec.edu.in>
2026-01-22 21:11:36 +0530

    Time: 1 ms (80.11%), Space: 42.9 MB (46.25%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i=0;
+        int j=0;
+        while(i<name.length() && j<typed.length()){
+            if(name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }else{
+                if(j > 0 && typed.charAt(j)==typed.charAt(j-1)){
+                    j++;
+                }else{
+                    return false;
+                }
+            }
+        }
+        while(j<typed.length()){
+            if(j > 0 && typed.charAt(j)==typed.charAt(j-1)){
+               j++;
+            }else{
+               return false;
+            }
+        }
+        return i==name.length();
+    }
+}
