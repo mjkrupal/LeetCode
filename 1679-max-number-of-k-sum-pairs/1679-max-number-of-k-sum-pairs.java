3fa36273a67697c8abf6682fb7e94d3ebe8ae7fb
JENNY KRUPAL MYLABATHULA <23a91a05b4@aec.edu.in>
2026-01-25 21:28:56 +0530

    Time: 24 ms (56.74%), Space: 69.3 MB (41.96%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        int left =0;
+        int right = nums.length-1;
+        int count=0;
+        Arrays.sort(nums);
+        while(left<right){
+            if(nums[left]+nums[right]==k){
+                count++;
+                left++;
+                right--;
+            }else if(nums[left]+nums[right]>k){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return count;
+    }
+}
