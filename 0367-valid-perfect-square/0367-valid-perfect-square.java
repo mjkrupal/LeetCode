ca45a36fe38cf992f951b166af598086ff5ef927
JENNY KRUPAL MYLABATHULA <23a91a05b4@aec.edu.in>
2026-02-13 22:45:46 +0530

    Time: 0 ms (100%), Space: 41 MB (99.93%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        int l=1;
+        int r=num/2;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            long power = (long) mid*mid;
+            if (power==num) return true;
+            else if(power>num) r=mid-1;
+            else l=mid+1;
+        }
+        return false;
+    }
+}
